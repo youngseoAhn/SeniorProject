30c5b157ec994ccfa47afcc9124e9585f03221df
Deanna Sithideth <101364792+deannasith@users.noreply.github.com>
2024-02-06 12:24:09 -0800

    added quick about page with css

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "../Components/Header";
+import './About.css';
 
 const About = () => {
     return (
         <div>
             <Header/>
-            <p>About Page</p>
+            <h1>About Safe Space</h1>
+            <section>
+
+            <h2 id="mission">Our Mission</h2>
+            <p>example paragraph </p>
+
+            <h2 id="items">Our Board Members</h2>
+            <p>list of members
+            </p>
+
+            </section>
+            
+            <Footer/>
         </div>
     )
 } 
