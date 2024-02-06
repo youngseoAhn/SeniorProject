861491532f261549dc6285ec8f1e4f0664e0a5db
mbbcrosby <mbcrosby@uw.edu>
2024-02-06 00:48:10 -0800

    final touches

--- src/Pages/Mental.js
+++ src/Pages/Mental.js
@@ -10,6 +10,7 @@ const Mental = () => {
             <h1 class='whatIsMH'>
                 What is Mental Health?
             </h1>
+<br></br>
 <h2 class='MHis'>
 Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act, 
 and helps determine how we handle stress, relate to others, and make choices.
@@ -17,15 +18,16 @@ and helps determine how we handle stress, relate to others, and make choices.
 Mental health is important at every stage of life, from childhood and adolescence through adulthood. Over the course of your life, 
 if you experience mental health problems, your thinking, mood, and behavior could be affected.
 </h2>
-            <h3 class='MHcond'>
-                Mental Health Condition:
-            </h3>
 <br></br>
-<body>
-        Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
-        <br></br>
 
-        <h4 class='MHfactors'>Many factors contribute to mental health conditions, including:</h4>
+<h3 class='MHcond'>
+Mental Health Conditions:
+<br></br>
+Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. 
+According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
+</h3>
+<h4 class='MHfactors'>Many factors contribute to mental health conditions, including:
+<br></br>
 
         -Biological factors, such as genes or brain chemistry
         <br></br>
@@ -33,9 +35,10 @@ if you experience mental health problems, your thinking, mood, and behavior coul
         <br></br>
         -Family history of mental health problems
         <br></br>
+        </h4>
 
-        <h5 class='MHtopics'>Some mental health topics include:</h5>
-
+        <h5 class='MHtopics'>Some mental health topics include:
+        <br></br>
         <br></br>
         -Antisocial personality disorder
         <br></br>
@@ -64,12 +67,15 @@ if you experience mental health problems, your thinking, mood, and behavior coul
         <br></br>
         A Serious Mental Illness (SMI) is a mental illness that interferes with a person’s life and ability to function. 
         Despite common misperceptions, having an SMI is not a choice, a weakness, or a character flaw. 
+        <br></br>
         It is not something that just “passes” or can be “snapped out of” with willpower.
         <br></br>
         <br></br>
-        View SAMHSA's Public Message about Serious Mental Illnesses.
-        https://www.samhsa.gov/serious-mental-illness
-        </body>
+        </h5>
+<h6 class='link'>
+View SAMHSA's Public Message about Serious Mental Illnesses.
+https://www.samhsa.gov/serious-mental-illness
+</h6>
         </div>
     )
 } 
