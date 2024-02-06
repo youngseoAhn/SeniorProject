821e09b281583c40bba744fa1e34bc7b1213bf98
mbbcrosby <mbcrosby@uw.edu>
2024-02-05 21:19:31 -0800

    mental css page added

--- src/Pages/Mental.js
+++ src/Pages/Mental.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
+import "./Mental.css"
 
 const Mental = () => {
     return (
         <div>
             <Header/>
             <p>Mental Health Matters. This is your Safe Space.</p>
+            
         </div>
     )
 } 
