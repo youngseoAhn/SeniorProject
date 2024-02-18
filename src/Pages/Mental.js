96f0546a090d5974cdb3048db9912da2336ab3e5
mbbcrosby <mbcrosby@uw.edu>
2024-02-17 23:23:29 -0800

    restructured coding/ renamed headers

--- src/Pages/Mental.js
+++ src/Pages/Mental.js
@@ -6,76 +6,89 @@ const Mental = () => {
     return (
         <div>
             <Header/>
-            <p class='heading'>Mental Health Matters. This is your Safe Space.</p>
-            <h1 class='whatIsMH'>
-                What is Mental Health?
-            </h1>
+<p class='mainheading'>Mental Health Matters. This is your Safe Space.</p>
+
+
+<h1 class='whatIsMH'>
+    What is Mental Health?
+</h1>
+
+<p1 class='MHis'>
+    Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act, 
+    and helps determine how we handle stress, relate to others, and make choices.
 <br></br>
-<h2 class='MHis'>
-Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act, 
-and helps determine how we handle stress, relate to others, and make choices.
-<br></br>
-Mental health is important at every stage of life, from childhood and adolescence through adulthood. Over the course of your life, 
-if you experience mental health problems, your thinking, mood, and behavior could be affected.
+    Mental health is important at every stage of life, from childhood and adolescence through adulthood. Over the course of your life, 
+    if you experience mental health problems, your thinking, mood, and behavior could be affected.
+</p1>
+
+
+<h2 class='MHcondHeader'>
+    Mental Health Conditions
 </h2>
-<br></br>
 
-<h3 class='MHcond'>
-Mental Health Conditions:
+<p2 class='MHcondexpl'>
 <br></br>
-Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. 
-According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
-</h3>
-<h4 class='MHfactors'>Many factors contribute to mental health conditions, including:
 <br></br>
+    Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. 
+    According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
+</p2>
 
-        -Biological factors, such as genes or brain chemistry
-        <br></br>
-        -Life experiences, such as trauma or abuse
-        <br></br>
-        -Family history of mental health problems
-        <br></br>
-        </h4>
 
-        <h5 class='MHtopics'>Some mental health topics include:
-        <br></br>
-        <br></br>
-        -Antisocial personality disorder
-        <br></br>
-        -Anxiety disorders (including generalized anxiety, panic disorders, obsessive-compulsive disorder (OCD), phobias, and social anxiety)
-        <br></br>
-        -Attention-deficit hyperactivity disorder (ADHD)
-        <br></br>
-        -Bipolar disorder
-        <br></br>
-        -Borderline Personality Disorder (BPD)
-        <br></br>
-        -Depression
-        <br></br>
-        -Eating disorders (including Anorexia Nervosa, binge eating Disorder, and Bulimia Nervosa)
-        <br></br>
-        -Post-traumatic Stress Disorder (PTSD)
-        <br></br>
-        -Schizophrenia
-        <br></br>
-        -Seasonal affective disorder (SAD)
-        <br></br>
-        -Self-harm
-        <br></br>
-        -Suicide and suicidal behavior
-        <br></br>
-        <br></br>
-        A Serious Mental Illness (SMI) is a mental illness that interferes with a person’s life and ability to function. 
-        Despite common misperceptions, having an SMI is not a choice, a weakness, or a character flaw. 
-        <br></br>
-        It is not something that just “passes” or can be “snapped out of” with willpower.
-        <br></br>
-        <br></br>
-        </h5>
-<h6 class='link'>
-View SAMHSA's Public Message about Serious Mental Illnesses.
-https://www.samhsa.gov/serious-mental-illness
-</h6>
+<h3 class='MHfactors'>Many factors contribute to mental health conditions, including: </h3>
+
+<p3 class='MHFacList'>
+<br></br>
+<br></br>
+    -Biological factors, such as genes or brain chemistry
+    <br></br>
+    -Life experiences, such as trauma or abuse
+    <br></br>
+    -Family history of mental health problems
+    <br></br>
+</p3>
+
+
+<h4 class='MHtopics'>Some mental health topics include: </h4>
+
+<p4 class="MHtopicList">
+    -Antisocial personality disorder
+    <br></br>
+    -Anxiety disorders (including generalized anxiety, panic disorders, obsessive-compulsive disorder (OCD), phobias, and social anxiety)
+    <br></br>
+    -Attention-deficit hyperactivity disorder (ADHD)
+    <br></br>
+    -Bipolar disorder
+    <br></br>
+    -Borderline Personality Disorder (BPD)
+    <br></br>
+    -Depression
+    <br></br>
+    -Eating disorders (including Anorexia Nervosa, binge eating Disorder, and Bulimia Nervosa)
+    <br></br>
+    -Post-traumatic Stress Disorder (PTSD)
+    <br></br>
+    -Schizophrenia
+    <br></br>
+    -Seasonal affective disorder (SAD)
+    <br></br>
+    -Self-harm
+    <br></br>
+    -Suicide and suicidal behavior
+</p4>
+
+
+<h5 class='SMI'>
+    A Serious Mental Illness (SMI) is a mental illness that interferes with a person’s life and ability to function. 
+    Despite common misperceptions, having an SMI is not a choice, a weakness, or a character flaw. 
+    <br></br>
+    It is not something that just “passes” or can be “snapped out of” with willpower.
+    <br></br>
+    <br></br>
+</h5>
+
+<a href ='https://www.samhsa.gov/serious-mental-illness' class='link'> View SAMHSA's Public Message about Serious Mental Illnesses. </a>
+
+
         </div>
     )
 } 
