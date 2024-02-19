db407ec23b0db3ef61f68c26c7600d5c264e281d
mbbcrosby <mbcrosby@uw.edu>
2024-02-18 16:00:13 -0800

    redid MH section layout and CSS, added image to work with

--- src/Pages/Mental.js
+++ src/Pages/Mental.js
@@ -6,75 +6,72 @@ const Mental = () => {
     return (
         <div>
             <Header/>
-<p class='mainheading'>Mental Health Matters. This is your Safe Space.</p>
+<p class='mainheading'> <br></br>Mental Health Matters. This is your Safe Space.</p>
+
+<section className="MH-section">
+                    <div className="whatIsMH">
+                        <h1 class='whatIsMHheader'>What is Mental Health? <br></br><br></br></h1>
+                        <p className="MHis">
+                        <br></br>
+                        Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act, 
+                        and helps determine how we handle stress, relate to others, and make choices.
+                        <br></br>
+                        Mental health is important at every stage of life, from childhood and adolescence through adulthood. Over the course of your life, 
+                        if you experience mental health problems, your thinking, mood, and behavior could be affected.
+                        </p>
+                        <img src="MHphoto1.png" alt="Mental Health Photo" className="MHphoto"/>
+                    </div>
+                </section>
+
+                <section className="MHcond-section">
+                    <div className="MHconds">
+                    <h2 class='MHcondHeader'> Mental Health Conditions</h2>
+                        <p className="MHcondexpl">
+                        Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. 
+                        According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
+                        </p>
+                    </div>
+                </section>
 
 
-<h1 class='whatIsMH'>
-    What is Mental Health?
-</h1>
+                <section className="MHfactors-section">
+                    <div className="MHfactors">
+                    <h3 class='MHfactorsheader'>Many factors contribute to mental health conditions, including: </h3>
+                        <p className="MHFacList">
+                        -Biological factors, such as genes or brain chemistry
+                        <br></br>
+                        -Life experiences, such as trauma or abuse
+                        <br></br>
+                        -Family history of mental health problems
+                        <br></br>
+                        </p>
+                    </div>
+                </section>
 
-<p1 class='MHis'>
-    Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act, 
-    and helps determine how we handle stress, relate to others, and make choices.
-<br></br>
-    Mental health is important at every stage of life, from childhood and adolescence through adulthood. Over the course of your life, 
-    if you experience mental health problems, your thinking, mood, and behavior could be affected.
-</p1>
+                <section className="MHtopics-section">
+                    <div className="MHtopics">
+                    <h4 class='MHtopicsheader'>Some mental health topics include:</h4>
+                        <p className="MHtopicList">
+                            <ul>
+                            <li>-Antisocial personality disorder</li>
+                            <li>-Anxiety disorders (including generalized anxiety, panic disorders, obsessive-compulsive disorder (OCD), 
+                                phobias, and social anxiety)</li>
+                            <li>-Attention-deficit hyperactivity disorder (ADHD)</li>
+                            <li>-Bipolar disorder</li>
+                            <li>-Borderline Personality Disorder (BPD)</li>
+                            <li>-Depression</li>
+                            <li>-Eating disorders (including Anorexia Nervosa, binge eating Disorder, and Bulimia Nervosa)</li>
+                            <li>-Post-traumatic Stress Disorder (PTSD)</li>
+                            <li>-Schizophrenia</li>
+                            <li>-Seasonal affective disorder (SAD)</li>
+                            <li>-Self-harm</li>
+                            <li>-Suicide and suicidal behavior</li>
+                            </ul>
+                        </p>
+                    </div>
+                </section>
 
 
-<h2 class='MHcondHeader'>
-    Mental Health Conditions
-</h2>
-
-<p2 class='MHcondexpl'>
-<br></br>
-<br></br>
-    Mental illnesses are disorders, ranging from mild to severe, that affect a person’s thinking, mood, and/or behavior. 
-    According to the National Institute of Mental Health, nearly one-in-five adults live with a mental illness.
-</p2>
-
-
-<h3 class='MHfactors'>Many factors contribute to mental health conditions, including: </h3>
-
-<p3 class='MHFacList'>
-<br></br>
-<br></br>
-    -Biological factors, such as genes or brain chemistry
-    <br></br>
-    -Life experiences, such as trauma or abuse
-    <br></br>
-    -Family history of mental health problems
-    <br></br>
-</p3>
-
-
-<h4 class='MHtopics'>Some mental health topics include: </h4>
-
-<p4 class="MHtopicList">
-    -Antisocial personality disorder
-    <br></br>
-    -Anxiety disorders (including generalized anxiety, panic disorders, obsessive-compulsive disorder (OCD), phobias, and social anxiety)
-    <br></br>
-    -Attention-deficit hyperactivity disorder (ADHD)
-    <br></br>
-    -Bipolar disorder
-    <br></br>
-    -Borderline Personality Disorder (BPD)
-    <br></br>
-    -Depression
-    <br></br>
-    -Eating disorders (including Anorexia Nervosa, binge eating Disorder, and Bulimia Nervosa)
-    <br></br>
-    -Post-traumatic Stress Disorder (PTSD)
-    <br></br>
-    -Schizophrenia
-    <br></br>
-    -Seasonal affective disorder (SAD)
-    <br></br>
-    -Self-harm
-    <br></br>
-    -Suicide and suicidal behavior
-</p4>
 
 
 <h5 class='SMI'>
@@ -86,7 +83,7 @@ const Mental = () => {
     <br></br>
 </h5>
 
-<a href ='https://www.samhsa.gov/serious-mental-illness' class='link'> View SAMHSA's Public Message about Serious Mental Illnesses. </a>
+<a href ='https://www.samhsa.gov/serious-mental-illness' class='MHlink'> <br></br>View SAMHSA's Public Message about Serious Mental Illnesses. </a>
 
 
         </div>
